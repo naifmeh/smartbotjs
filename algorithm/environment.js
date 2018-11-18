13d1fc5b346b42ba9a802a3c06201fffca60e87b
naifmeh <naif.meh@gmail.com>
2018-11-18 20:18:16 +0100

    crawler done

---
+++ algorithm/environment.js
@@ -0,0 +1,7 @@
+
+function EnvironmentController() {
+
+
+}
+
+module.exports = EnvironmentController();
---
+++ crawler/crawler.js
@@ -0,0 +1,92 @@
+
+
+
+function CrawlerController() {
+    let proxy;
+    let user_agent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36";
+    let url;
+    let viewportType = "desktop";
+    let loadPictures = true;
+    let runCss = true;
+    let plugins = '';
+
+    function set_proxy(new_proxy) {
+        proxy = new_proxy;
+    }
+
+    function set_useragent(new_ua) {
+        user_agent = new_ua;
+    }
+
+    function set_url(new_url) {
+        url = new_url;
+    }
+
+    function should_load_pictures(load_pics) {
+        loadPictures = load_pics
+    }
+
+    function should_run_css(run_css) {
+        runCss = run_css
+    }
+
+    function set_viewport(vp_type) {
+        //Different websites viewport
+    }
+
+    function set_plugins(set_plugins) {
+        plugins = set_plugins
+    }
+
+    async function launch_crawler(action='screenshot', debug=false) {
+        const puppeteer = require('puppeteer');
+
+        let properties = {};
+        if(debug) {
+            properties.headless = true;
+        }
+        if(user_agent !== undefined) {
+            properties.args.push(`--user-agent=${user_agent}`);
+        }
+        if(proxy !== undefined) {
+            properties.args.push(`--proxy-server=${proxy}`);
+        }
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        //page.setUserAgent(user_agent);
+
+        if(url === undefined) {
+            throw new Error("URL not defined");
+        }
+        if(!loadPictures || !runCss) {
+            await page.setRequestInterception(true);
+            if (!loadPictures) {
+                page.on('request', (req) => {
+                    if (req.resourceType() === 'image') {
+                        req.abort();
+                    } else {
+                        req.continue();
+                    }
+                });
+            }
+            if (!runCss) {
+                page.on('request', (req) => {
+                    if(req.resourceType() === 'stylesheet') {
+                        req.abort();
+                    } else {
+                        req.continue();
+                    }
+                });
+            }
+        }
+        const response = await page.goto(url);
+        if(action === 'screenshot') {
+            await page.screenshot({path: `screenshots/${url}.png`});
+        }
+
+        await browser.close();
+
+        return Promise.resolve(response);
+    }
+}
+module.exports = CrawlerController()
